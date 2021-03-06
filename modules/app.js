3ebcb078623fd68fb44608cc8142286c721dd653
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-03-06 18:01:41 +0100

    renamed file
    installed bootstrap
    Added api call for wikipedia page

--- modules/app.js
+++ modules/app.js
@@ -1,7 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const inputElement = document.querySelector('input');
+const inputElement = document.querySelector('.search-bar');
 
 inputElement.addEventListener('keyup', (event) => {
   console.log('value:', event.target.value);
+  const api = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search={event.target.value}`
+    document.addEventListener('keyup', (e) => {
+       // Get random article
+// get random word
+    if (e.key ===  "Enter"){
+        window.location.href = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=bears";
+     }
+    })
 });
